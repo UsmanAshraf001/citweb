0158413b1e0820e957739e410ae4f7e98b71d918
M.Usman <usmanmuhammadashraf@gmail.com>
2026-05-07 16:57:10 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+$name = "Usman";
+$age = 24;
+
+echo "<h2>Welcome to PHP</h2>";
+echo "Name: " . $name . "<br>";
+echo "Age: " . $age . "<br>";
+
+if ($age >= 18) {
+    echo "Status: Adult";
+} else {
+    echo "Status: Minor";
+}
+
+?>
+
+</body>
+</html>
